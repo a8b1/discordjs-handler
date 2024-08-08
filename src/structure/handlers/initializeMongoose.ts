2dc3f7373e70bb81a3b838285621a43de76046de
Potato <discodingpotato@gmail.com>
2024-08-08 12:43:01 +0530

    mongoose initialization

--- src/structure/handlers/initializeMongoose.ts
+++ src/structure/handlers/initializeMongoose.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const initializeMongoose = () => {
     mongoose.set('strictQuery', false);
-    mongoose.connect(process.env.MONGO_URI, {
+    mongoose.connect(process.env.MONGO_URI as string, {
         dbName: 'catvroom'
     })
 };
