5df30f57c26a8af1e111a422026ea71d3713e152
discodingpotato <discodingpotato@gmail.com>
2024-08-10 01:37:17 +0530

    context menu interaction

--- src/structure/events/guild/interactionCreate.ts
+++ src/structure/events/guild/interactionCreate.ts
@@ -2,6 +2,10 @@ import { Apple } from "../../utils/Apple";
 
 export default (client: Apple) => {
     client.on('interactionCreate', async interaction => {
+        if(interaction.isContextMenuCommand()) {
+            const command = client.collections.interactionCommands.get(interaction.commandName);
+            if(command) await command.run(client, interaction as any);
+        }
         if(interaction.isCommand()) {
             const command = client.collections.interactionCommands.get(interaction.commandName);
             if(!command) return;
